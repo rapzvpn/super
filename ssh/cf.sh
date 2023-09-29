0ee7a66630fd9277bf545b0c3faff38f8626191e
rapzvpn <143205129+rapzvpn@users.noreply.github.com>
2023-09-30 02:56:35 +0700

    Add files via upload

--- ssh/cf.sh
+++ ssh/cf.sh
@@ -1,44 +1,49 @@
 #!/bin/bash
-# Edition : Stable Edition V3.0
-# Auther  : rapzvpn
-# (C) Copyright 2023
-# =========================================
+# // String / Request Data
+# Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
+#MYIP=$(wget -qO- https://ipv4.icanhazip.com);
+#MYIP=$(wget -qO- https://ipv6.icanhazip.com);
 clear
-apt install jq curl -y >/dev/null 2>&1
+apt install jq curl -y
+sub=$(</dev/urandom tr -dc a-z | head -c4)
 DOMAIN=madurakocak.my.id
-sub=$(</dev/urandom tr -dc a-x1-9 | head -c5 | tr -d '\r' | tr -d '\r\n')
 SUB_DOMAIN=${sub}vip.madurakocak.my.id
 CF_ID=shabudin039@gmail.com
 CF_KEY=746762286a7ac6f54e7c51565e2b19a4c1ab8
 set -euo pipefail
-IP=$(wget -qO- ifconfig.me/ip);
+IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/super/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/super/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
 if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/super/zones/${ZONE}/dns_records" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
 fi
 
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/super/zones/${ZONE}/dns_records/${RECORD}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+     
 echo "Host : $SUB_DOMAIN"
-echo "IP=" >> /var/lib/rapzvpn/ipvps.conf
-echo $SUB_DOMAIN > /etc/xray/domain
 echo $SUB_DOMAIN > /root/domain
-rm -f /root/cf.sh
+echo "IP=$SUB_DOMAIN" > /var/lib/SIJA/ipvps.conf
+sleep 1
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+yellow "Domain added.."
+sleep 3
+domain=$(cat /root/domain)
+cp -r /root/domain /etc/xray/domain
